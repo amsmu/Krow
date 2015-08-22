55930dd0815863a7655bb4c5cad45a966e2e3574
Amarjeet Singh Mudhar <amarjeetsingh75@gmail.com>
2015-08-23 01:12:16 +0530

    destroy todo item and tests" "

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,15 +1,15 @@
 class TodoItemsController < ApplicationController
+  
+  before_action :find_todo_list
+
   def index
-  	@todo_list = TodoList.find(params[:todo_list_id])
   end
 
   def new
-  	@todo_list = TodoList.find(params[:todo_list_id])
   	@todo_item = @todo_list.todo_items.new
   end
 
   def create
-  	@todo_list = TodoList.find(params[:todo_list_id])
   	@todo_item = @todo_list.todo_items.new(todo_item_params)
   	if @todo_item.save
   		flash[:success] = "Added todo item successfully."
@@ -19,8 +19,8 @@ class TodoItemsController < ApplicationController
   		render action: :new
   	end
   end
+
   def update
-    @todo_list = TodoList.find(params[:todo_list_id])
     @todo_item = @todo_list.todo_items.find(params[:id])
     if @todo_item.update_attributes(todo_item_params)
       flash[:success] = "Added todo item successfully."
@@ -30,13 +30,28 @@ class TodoItemsController < ApplicationController
       render action: :edit
     end
   end
+  
+  def edit
+    @todo_item = @todo_list.todo_items.find(params[:id])
+  end
 
+  def destroy
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    if @todo_item.destroy
+      flash[:success] = "Todo list item successfully deleted."
+    else
+      flash[:error] = "Todo list item could not be deleted."
+    end
+    redirect_to todo_list_todo_items_path
+  end
+  
   def url_options
     {todo_list_id: params[:todo_list_id]}.merge(super)
   end
-  def edit
+  
+
+  def find_todo_list
     @todo_list = TodoList.find(params[:todo_list_id])
-    @todo_item = @todo_list.todo_items.find(params[:id])
   end
 
   private
---
+++ spec/features/todo_items/destroy_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe "Deleting todo items" do
+	let!(:todo_list){TodoList.create(title: "Gadgets", description: "Gadgets to buy list.")}
+	let!(:todo_item){todo_list.todo_items.create(content: "Note 5")}
+
+
+	def visit_todo_list(list)
+		visit "/todo_lists"
+		within "#todo_list_#{list.id}" do
+			click_link "List Items"
+		end
+	end
+
+	it "Is successful when item is deleted" do
+		visit_todo_list(todo_list)
+		within("#todo_item_#{todo_item.id}") do
+			click_link "Delete"
+		end
+		expect(page).to have_content("Todo list item successfully deleted.")
+		expect(page).to_not have_content("Note 5")
+
+	end
+
+end
