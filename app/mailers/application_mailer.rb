2624816208c7f78954303936e6c32abe6194897c
Amarjeet Singh Mudhar <amarjeetsingh75@gmail.com>
2015-12-27 22:42:09 +0530

    added email changed

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
 	default_url_options[:host] = "rocky-caverns-6007.herokuapp.com/"
-	default from: "noreply@teksavant.com"
+	default from: "amarjeetsingh755@teksavant.com"
 	layout 'mailer'
 
 	def password_reset(user)
